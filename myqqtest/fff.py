26852d2421143e1e6e87d56acada1494122244d4
zhongqian <qian.z@163.com>
2016-10-28 15:46:52 +0800

    ec test

---
+++ myqqtest/fff.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+"""
+Created on 2016年10月28日  下午3:45:19
+@author: Administrator
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
